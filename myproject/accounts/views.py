3201bd28b3ca2a2565dad02c655d4e5746a144e7
Silva <deboraaaline21@gmail.com>
2024-08-15 20:29:36 -0300

    Fix

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,6 +1,5 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-
 from accounts.models import User
 from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -14,3 +13,4 @@ class RegisterView(generics.CreateAPIView):
 
 class LoginView(TokenObtainPairView):
     serializer_class = LoginSerializer
+
